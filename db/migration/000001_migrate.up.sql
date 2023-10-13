f1da0416590b63dc1a27d5d617995319c4350959
Phadoo131 <narapong.131@gmail.com>
2023-10-14 00:30:49 +0700

    Add db/migration

---
+++ db/migration/000001_migrate.up.sql
@@ -0,0 +1,39 @@
+CREATE TABLE "Client" (
+  "ClientID" serial PRIMARY KEY,
+  "Name" VARCHAR(255),
+  "Email" VARCHAR(255),
+  "Phone_Number" VARCHAR(20),
+  "Address" TEXT
+);
+
+CREATE TABLE "Booker" (
+  "BookerID" serial PRIMARY KEY,
+  "Name" VARCHAR(255),
+  "Email" VARCHAR(255),
+  "Phone_Number" VARCHAR(20),
+  "Address" TEXT
+);
+
+CREATE TABLE "Restaurant" (
+  "RestaurantID" serial PRIMARY KEY,
+  "Name" VARCHAR(255),
+  "Location" VARCHAR(255),
+  "Contact_Number" VARCHAR(20)
+);
+
+CREATE TABLE "Booking" (
+  "BookingID" serial PRIMARY KEY,
+  "ClientID" INT REFERENCES "Client" ("ClientID"),
+  "BookerID" INT REFERENCES "Booker" ("BookerID"),
+  "RestaurantID" INT REFERENCES "Restaurant" ("RestaurantID"),
+  "Date_and_Time" timestamptz,
+  "Status" VARCHAR(20)
+);
+
+CREATE TABLE "ChatMessage" (
+  "MessageID" serial PRIMARY KEY,
+  "SenderID" INT REFERENCES "Client" ("ClientID"),
+  "ReceiverID" INT REFERENCES "Client" ("ClientID"),
+  "MessageText" TEXT,
+  "Timestamp" timestamptz
+);
